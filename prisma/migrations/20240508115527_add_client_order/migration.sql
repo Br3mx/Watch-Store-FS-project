bb9ef62c5e2eb5e58a772b1b1d18f8eecc408347
Przemysław Bulanda <devprzemas@gmail.com>
2024-05-08 14:20:36 +0200

    Add seed.ts

---
+++ prisma/migrations/20240508115527_add_client_order/migration.sql
@@ -0,0 +1,64 @@
+-- CreateTable
+CREATE TABLE `Watch` (
+    `id` VARCHAR(191) NOT NULL,
+    `price` INTEGER NOT NULL,
+    `oldPrice` VARCHAR(191) NULL,
+    `mainImg` VARCHAR(191) NOT NULL,
+    `restImg` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `model` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Client` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `address` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Order` (
+    `id` VARCHAR(191) NOT NULL,
+    `productId` VARCHAR(191) NULL,
+    `clientId` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `role` ENUM('USER', 'ADMIN') NOT NULL DEFAULT 'USER',
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Password` (
+    `id` VARCHAR(191) NOT NULL,
+    `hashedPassword` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Password_userId_key`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Order` ADD CONSTRAINT `Order_productId_fkey` FOREIGN KEY (`productId`) REFERENCES `Watch`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Order` ADD CONSTRAINT `Order_clientId_fkey` FOREIGN KEY (`clientId`) REFERENCES `Client`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Password` ADD CONSTRAINT `Password_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ prisma/seed.ts
@@ -0,0 +1,138 @@
+import { PrismaClient } from '@prisma/client';
+const db = new PrismaClient();
+
+function getWatches() {
+  return [
+    {
+      id: 'fd105551-0f0d-4a9f-bc41-c559c8a17256',
+      price: 990,
+      oldPrice: 1100,
+      mainImg: '',
+      restImg: '',
+      name: 'ADRIATICA CLASSIC',
+      model: 'A3601.1111QFZ',
+      description:
+        'Zegarek damski Adriatica A3601.1111QFZ to wyrafinowany wybór dla kobiety, która szuka połączenia elegancji i precyzji szwajcarskiego rzemiosła. Koperta wykonana z wysokiej jakości stali szlachetnej 316L, pokryta złotym odcieniem, jest wykończona zdobieniami z kryształków Swarovskiego, dodając zegarkowi luksusowego blasku. Tarcza wzbogacona o funkcjonalne sub-tarcze z datownikiem dni miesiąca i tygodnia oraz cykl księżycowy, co sprawia, że zegarek jest nie tylko piękny, ale i użyteczny. Wytrzymałe szafirowe szkiełko gwarantuje ochronę przed uszkodzeniami, a bransoleta, również ze stali szlachetnej, łączy w sobie wytrzymałość i ponadczasową estetykę. Mechanizm kwarcowy Ronda 706 SWISS MADE zapewnia niezawodną precyzję, a wodoszczelność do 50 metrów pozwala na bezproblemowe użytkowanie w codziennych sytuacjach',
+    },
+    {
+      id: 'fd105551-0f0d-4a9f-bc41-c559c8a17789',
+      price: 1276,
+      oldPrice: 1450,
+      mainImg: '',
+      restImg: '',
+      name: 'LUMINOX',
+      model: 'XS.0321.BO.L',
+      description:
+        'Luminox Leatherback Sea Turtle Giant 0320 XS.0321.BO.L to kolejna propozycja marki Luminox, znanej z survivalowych modeli. Ten casualowy zegarek męski działa w oparciu o precyzyjny mechanizm kwarcowy Ronda 515 Swiss Made. Na analogowej tarczy umieszczone zostały datownik dni miesiąca, subtarcza czasu dobowego oraz silne podświetlenie trytowe. Całość zabezpieczona jest utwardzanym szkłem mineralnym, osadzona w kopercie z CARBONOX-u i umocowana na gumowym pasku. Urządzenie utrzymano w czarnej kolorystyce. Wodoszczelność rzędu 100 metrów zapewnia ochronę zegarka podczas pływania powierzchniowego. Gwarancja producenta wynosi 2 lata na cały model i 10 lat na podświetlenie trytowe.',
+    },
+    {
+      id: 'fd105551-0f0d-4a9f-bc41-c559c8a19890',
+      price: 3136,
+      oldPrice: 3690,
+      mainImg: '',
+      restImg: '',
+      name: 'FREDERIQUE CONSTANT',
+      model: 'FC-220SS5B6',
+      description:
+        'Zegarek męski Frederique Constant Classics FC-220SS5B6 to elegancki model pasujący do garnituru. Jednak doskonale dopełni również mniej oficjalny ubiór. Delikatny wygląd to zasługa połączenia srebrnej koperty ze stali szlachetnej ze srebrną tarczą zdobioną szlifem słonecznym. Klasyczny design dopełniono brązowym paskiem uszytym ze skóry, który zapewnia tym samym doskonałą jakość i wygodę użytkowania. Użytkownik zegarka nie musi się martwić o zachlapanie urządzenia. Do zakupu dołączona jest 2-letnia gwarancja producenta oraz oryginalne pudełko.',
+    },
+    {
+      id: 'fd105551-0f0d-4a9f-bc41-c89378a17297',
+      price: 2600,
+      oldPrice: undefined,
+      mainImg: '',
+      restImg: '',
+      name: 'TISSOT',
+      model: 'T006.407.16.033.00',
+      description:
+        'Zegarek męski Tissot T006.407.16.033.00 z serii Le Locle to model ceniony na całym świecie. Wyposażono go w mechanizm automatyczny Powermatic 80 Swiss Made oraz twarde szafirowe szkiełko. Wodoszczelność produktu wynosi 30 metrów. Koperta zegarka T0064071603300 została wykonana z wysokiej jakości stali szlachetnej 316L. Gwarancja producenta liczy 2 lata i jest ważna od daty zakupu',
+    },
+    {
+      id: 'fd105551-0f0d-4ag6-bc41-c559c8a17256',
+      price: 3600,
+      oldPrice: undefined,
+      mainImg: '',
+      restImg: '',
+      name: 'CERTINA',
+      model: 'C033.051.22.118.01',
+      description:
+        'Tradycyjne szwajcarskie zegarmistrzostwo, klasyczna kolorystyka, materiały premium i nowoczesne rozwiązania – taki właśnie jest damski zegarek DS-8 Lady COSC C033.051.22.118.01 od marki Certina. Produkt, dzięki srebrno-złotej stylistyce, wkomponuje się w wiele formalnych i codziennych stylizacji. Subtelny kobiecy projekt podkreśla tarcza z masy perłowej. Model jest wysokiej klasy chronometrem wyposażonym w mechanizm kwarcowy z technologią Precidrive™ i funkcją datownika. Jego precyzję potwierdza certyfikat COSC. Zegarek Certina C0330512211801 został wyposażony w system zabezpieczeń Double Security. Jest wodoszczelny w klasie 10 ATM.',
+    },
+  ];
+}
+
+function getClients() {
+  return [
+    {
+      id: 'fd105551-0f0d-4a9f-bc41-c559c8a17201',
+      name: 'John Doe',
+      address: '123 Main Street, London',
+    },
+    {
+      id: 'fd105551-0f0d-4a9f-bc41-c559c8a17202',
+      name: 'Jane Doe',
+      address: 'Baker Street 12B, New York',
+    },
+    {
+      id: 'fd105551-0f0d-4a9f-bc41-c559c8a17203',
+      name: 'Thomas Jefferson',
+      address: '321 Main Street, London',
+    },
+  ];
+}
+
+function getOrders() {
+  return [
+    {
+      id: 'fd105551-0f0d-4a9f-bc41-c559c8a17260',
+      productId: 'fd105551-0f0d-4a9f-bc41-c559c8a17256',
+      clientId: 'fd105551-0f0d-4a9f-bc41-c559c8a17203',
+    },
+    {
+      id: 'fd105551-0f0d-4a9f-bc41-c559c8a17261',
+      productId: 'fd105551-0f0d-4a9f-bc41-c89378a17297',
+      clientId: 'fd105551-0f0d-4a9f-bc41-c559c8a17202',
+    },
+    {
+      id: 'fd105551-0f0d-4a9f-bc41-c559c8a17262',
+      productId: 'fd105551-0f0d-4a9f-bc41-c89378a17297',
+      clientId: 'fd105551-0f0d-4a9f-bc41-c559c8a17201',
+    },
+  ];
+}
+
+async function seed() {
+  await db.watch.deleteMany();
+  await db.client.deleteMany();
+  await db.order.deleteMany();
+
+  await Promise.all(
+    getWatches().map((watch) => {
+      return db.watch.create({ data: watch });
+    }),
+  );
+
+  await Promise.all(
+    getClients().map((client) => {
+      return db.client.create({ data: client });
+    }),
+  );
+
+  await Promise.all(
+    getOrders().map(({ productId, clientId, ...orderData }) => {
+      return db.order.create({
+        data: {
+          ...orderData,
+          product: {
+            connect: { id: productId },
+          },
+          client: {
+            connect: { id: clientId },
+          },
+        },
+      });
+    }),
+  );
+}
+
+seed();
