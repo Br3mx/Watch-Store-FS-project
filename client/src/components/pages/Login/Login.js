f99ee8af2c8e62e6949fd2cbac9b471371b0415d
Przemysław Bulanda <devprzemas@gmail.com>
2024-05-11 01:32:53 +0200

    Commit for help

--- client/src/components/pages/Login/Login.js
+++ client/src/components/pages/Login/Login.js
@@ -1,4 +1,20 @@
+import { Col, Row } from 'react-bootstrap';
+import LoginForm from '../../features/LoginForm/LoginForm';
+
 const Login = () => {
-  return <>Login</>;
+  return (
+    <>
+      <Row>
+        <Col xs={12} md={8} lg={6} className="mx-auto">
+          <h1 className="text-center">Sign up</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12} md={8} lg={6} className="mx-auto">
+          <LoginForm />
+        </Col>
+      </Row>
+    </>
+  );
 };
 export default Login;
